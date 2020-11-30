251330ae2890bd79de99720583a1660679ab4d18
Clemens Tolboom <clemens@build2be.com>
2020-11-30 13:41:03 +0100

    #10: Add wave manager for 3D.

---
+++ addons/SpawnPoints/WaveManager.gd
@@ -0,0 +1,62 @@
+tool
+extends Node
+
+class_name WaveManager
+
+export (Array, Dictionary) var waves = [
+	{
+		"size": 3,
+		"capacity": 10,
+	},{
+		"size": 2,
+		"capacity": 6,
+	},
+]
+
+var spawn_point: SpawnPoint
+var wave_manager: WaveManagerClass
+
+func _ready():
+	if not has_valid_spawn_point():
+		return
+
+	spawn_point = get_parent()
+
+	if Engine.editor_hint:
+		# We don't need a wavemanager object when editing
+		return
+
+	spawn_point.connect("is_depleted", self, "_on_SpawnPoint_is_depleted")
+
+	wave_manager = WaveManagerClass.new(waves)
+	wave_manager.cleanup_waves()
+
+func _get_configuration_warning():
+	if not has_valid_spawn_point():
+		return "Parent must be a SpawnPoint"
+	return ""
+
+func has_valid_spawn_point() -> bool:
+	var p = get_parent()
+	return p is SpawnPoint || not spawn_point == null
+
+func _process(delta):
+	if Engine.editor_hint:
+		update_configuration_warning()
+		# Static call
+		WaveManagerClass.add_empty(waves)
+
+		# Sync SpawnPoint with first wave data
+		if has_valid_spawn_point():
+			var p = get_parent()
+			p.wave_size = waves[0].size
+			p.capacity = waves[0].capacity
+
+func config_spawn_point():
+	wave_manager.config_spawn_point(spawn_point)
+
+func _on_SpawnPoint_is_depleted(who):
+	if who != spawn_point:
+		return
+
+	config_spawn_point()
