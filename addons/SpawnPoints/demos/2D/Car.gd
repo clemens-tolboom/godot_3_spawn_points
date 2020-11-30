3c3c4aff086b3fa6df7565217d7f5a7fbb517d17
Clemens Tolboom <clemens@build2be.com>
2020-11-30 11:28:27 +0100

    Stop car when depleted.

--- addons/SpawnPoints/demos/2D/Car.gd
+++ addons/SpawnPoints/demos/2D/Car.gd
@@ -5,7 +5,6 @@ var pos:Vector2 = Vector2(0,450)
 var vel:Vector2 = Vector2(1,0)
 
 func _process(delta):
-
 	set_global_position(get_global_position() + vel)
 
 
@@ -13,3 +12,7 @@ func _on_SpawnPoint2D_item_placed(item):
 	# Try not too overlap the items
 	item.rect_position.x += rand_range(-20, 20)
 	item.rect_position.y += rand_range(-20, 20)
+
+
+func _on_SpawnPoint2D_is_depleted(who):
+	vel = Vector2.ZERO
